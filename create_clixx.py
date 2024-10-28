117048fa9aefe4303a91f2cf99fcb75ffcccb7da
das40 <oyeyioladasola30@gmail.com>
2024-10-28 12:18:18 +1100

    Update create_clixx.py

--- create_clixx.py
+++ create_clixx.py
@@ -61,7 +61,7 @@ ami_id = "ami-00f251754ac5da7f0"
 instance_type = "t2.micro"
 key_pair_name = "stack_devops_kp"
 certificate_arn = "arn:aws:acm:us-east-1:619071313311:certificate/ed0a7048-b2f1-4ca7-835d-06d5cc51f805"
-hosted_zone_id = "Z032607324NJ585T59J7F"
+hosted_zone_id = "Z0881876FFUR3OKRNM20"
 record_name = "dev.clixx-dasola.com"
 aws_region = "us-east-1"
 
