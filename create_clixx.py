6418e8c6c8243806cc2928c5092283633c622c36
das40 <oyeyioladasola30@gmail.com>
2024-10-29 15:10:46 +1100

    Update create_clixx.py

--- create_clixx.py
+++ create_clixx.py
@@ -459,9 +459,8 @@ else:
     print(f"Route 53 record already exists for {clixx_record_name}")
 
 # Encode the user data to Base64
-
 # Encode the user data to Base64
-clixx_user_data_script= f'''#!/bin/bash -x
+clixx_user_data_script = f'''#!/bin/bash -x
 # Basic logging
 exec > >(tee /var/log/userdata.log) 2>&1
 
@@ -474,7 +473,7 @@ efs_name="CLiXX-EFS"
 clixx_file_system_id="{clixx_file_system_id}"
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
 MOUNT_POINT="/var/www/html"
-RECORD_NAME="${record_name}"  # Define RECORD_NAME here
+RECORD_NAME="${{record_name}}"  # Define RECORD_NAME here
 
 # Update packages and install dependencies
 sudo yum update -y
@@ -490,8 +489,8 @@ sudo systemctl enable httpd
 sudo usermod -a -G apache ec2-user
 sudo chown -R ec2-user:apache /var/www
 sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
+find /var/www -type d -exec sudo chmod 2775 {{}} \;
+find /var/www -type f -exec sudo chmod 0664 {{}} \;
 
 # Mount EFS
 AVAILABILITY_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
@@ -533,7 +532,7 @@ else
 fi
 
 # Replace placeholders in wp-config.php with actual values
-sed -i "s/database_name_here/${DB_NAME}/; s/username_here/${DB_USER}/; s/password_here/${DB_USER_PASSWORD}/; s/localhost/${DB_HOST}/" wp-config.php
+sed -i "s/database_name_here/${{DB_NAME}}/; s/username_here/${{DB_USER}}/; s/password_here/${{DB_USER_PASSWORD}}/; s/localhost/${{DB_HOST}}/" wp-config.php
 
 # Add HTTPS enforcement
 sudo sed -i "81i if (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {{ \$_SERVER['HTTPS'] = 'on'; }}" wp-config.php
@@ -541,10 +540,10 @@ sudo sed -i "81i if (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HT
 # Set WordPress options using RECORD_NAME
 if [ -n "$RECORD_NAME" ]; then
     mysql -u $DB_USER -p$DB_USER_PASSWORD -h $DB_HOST -D $DB_NAME -e "
-        UPDATE wp_options SET option_value='https://${RECORD_NAME}' WHERE option_name='home';
-        UPDATE wp_options SET option_value='https://${RECORD_NAME}' WHERE option_name='siteurl';
-        UPDATE wp_options SET option_value='https://${RECORD_NAME}' WHERE option_name='ping_sites';
-        UPDATE wp_options SET option_value='https://${RECORD_NAME}' WHERE option_name='open_shop_header_retina_logo';
+        UPDATE wp_options SET option_value='https://${{RECORD_NAME}}' WHERE option_name='home';
+        UPDATE wp_options SET option_value='https://${{RECORD_NAME}}' WHERE option_name='siteurl';
+        UPDATE wp_options SET option_value='https://${{RECORD_NAME}}' WHERE option_name='ping_sites';
+        UPDATE wp_options SET option_value='https://${{RECORD_NAME}}' WHERE option_name='open_shop_header_retina_logo';
     "
     echo "WordPress options updated with RECORD_NAME: $RECORD_NAME"
 else
@@ -558,8 +557,8 @@ sudo sed -i '151s/None/All/' /etc/httpd/conf/httpd.conf
 sudo chown -R apache /var/www
 sudo chgrp -R apache /var/www
 sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
+find /var/www -type d -exec sudo chmod 2775 {{}} \;
+find /var/www -type f -exec sudo chmod 0664 {{}} \;
 
 # Restart and enable Apache
 sudo systemctl restart httpd
@@ -576,8 +575,8 @@ sudo sed -i "s/database_name_here/$DB_NAME/; s/username_here/$DB_USER/; s/passwo
 
 # Adjust permissions for WordPress
 sudo chown -R apache:apache "$MOUNT_POINT"
-sudo find "$MOUNT_POINT" -type d -exec chmod 755 {} \;
-sudo find "$MOUNT_POINT" -type f -exec chmod 644 {} \;
+sudo find "$MOUNT_POINT" -type d -exec chmod 755 {{}} \;
+sudo find "$MOUNT_POINT" -type f -exec chmod 644 {{}} \;
 
 # Reload Apache
 sudo systemctl restart httpd
